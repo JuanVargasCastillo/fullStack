6cb6d874dee40ec058c601889f3de4aa38a26203
Benjamin Valdes <benjissj.vald15@gmail.com>
2025-06-10 20:44:13 -0400

    Venta y Envios listo, error en vendedores

--- ventas-api-spring-boot/src/main/java/com/ventas/controllers/VentaController.java
+++ ventas-api-spring-boot/src/main/java/com/ventas/controllers/VentaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/ventas")
+@RequestMapping("/api/ventas")
 public class VentaController {
     @Autowired
     private VentaService ventaService;
@@ -19,17 +19,17 @@ public class VentaController {
         return ResponseEntity.ok(ventaService.crearVenta(ventaDTO));
     }
 
-    @GetMapping("api/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<?> obtenerVenta(@PathVariable Long id) {
         return ResponseEntity.ok(ventaService.obtenerVenta(id));
     }
 
-    @GetMapping("api/cliente/{clienteId}")
+    @GetMapping("/cliente/{clienteId}")
     public ResponseEntity<?> ventasPorCliente(@PathVariable Integer clienteId) {
         return ResponseEntity.ok(ventaService.obtenerVentasPorCliente(clienteId));
     }
 
-    @PostMapping("api/cupones/validar")
+    @PostMapping("/cupones/validar")
     public ResponseEntity<?> validarCupon(@RequestBody CuponValidacionDTO cupon) {
         return ResponseEntity.ok(ventaService.validarCupon(cupon));
     }
